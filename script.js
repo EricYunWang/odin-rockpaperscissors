91cd406cd49b23080198ac86767cd57626fbe173
EricYunWang <ericwang3@hotmail.com>
2026-02-15 00:58:46 +0000

    Add random computer choice function
    The getComputerChoice() function generates a random number
    between 0 to 2. 0 returns rock, 1 returns paper, 2 returns scissors.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World1");
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0){
+        return "Rock";
+    }
+    else if (num === 1){
+        return "Paper";
+    }
+    else return "Scissors";
+}
+
+let comChoice = getComputerChoice();
+console.log(comChoice);
