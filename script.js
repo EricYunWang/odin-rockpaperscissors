a4ae1190754f509809eda9ca0ea55ad449439a69
EricYunWang <ericwang3@hotmail.com>
2026-02-15 01:12:11 +0000

    Add user choice function
    Added function getHumanChoice() to prompt the user for an input.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if (num === 0){
         return "Rock";
@@ -8,6 +8,14 @@ function getComputerChoice() {
     }
     else return "Scissors";
 }
+
+function getHumanChoice(){
+    let userInput = prompt("What is your choice? Please type Rock, Paper, or Scissors.");
+    return userInput;
+}
+
+let humChoice = getHumanChoice();
+console.log(humChoice);
 
 let comChoice = getComputerChoice();
 console.log(comChoice);
