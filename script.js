f95c3a35be9230b74b627982bb7d8de2916015da
EricYunWang <ericwang3@hotmail.com>
2026-02-15 01:33:53 +0000

    Add single round gameplay
    Added function playRound that takes the 2 choices as arguments.
    Based on the player choice and the random computer choice comparison,
    the winner will get 1 score.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if (num === 0){
-        return "Rock";
+        return "rock";
     }
     else if (num === 1){
-        return "Paper";
+        return "paper";
     }
-    else return "Scissors";
+    else return "scissors";
 }
 
 function getHumanChoice(){
@@ -14,8 +14,31 @@ function getHumanChoice(){
     return userInput;
 }
 
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice === computerChoice){
+        console.log("Tie");
+    }
+    else if ((humanChoice === "rock" && computerChoice === "paper") || 
+             (humanChoice === "paper" && computerChoice === "scissors") ||
+             (humanChoice === "scissors" && computerChoice === "rock")){
+        console.log("You lose, " + computerChoice + " beats " + humanChoice);
+        computerScore++;
+    }
+    else {
+        console.log("You win, " + humanChoice + " beats " + computerChoice);
+        humanScore++;
+    }
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
 let humChoice = getHumanChoice();
 console.log(humChoice);
 
 let comChoice = getComputerChoice();
 console.log(comChoice);
+
+playRound(humChoice, comChoice);
